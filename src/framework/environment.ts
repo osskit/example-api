0b245504d09609dc5d59b12a1af40d9462da039e
Sharon Grossman <sharong1337@gmail.com>
2022-07-06 13:55:27 +0300

    name

--- src/framework/environment.ts
+++ src/framework/environment.ts
@@ -1,7 +1,7 @@
 import env from 'env-var';
 
 export const nodeEnv = env.get('NODE_ENV').required().asString();
-export const serviceName = 'example-api';
+export const serviceName = env.get('SERVICE_NAME').required().asString();
 export const port = env.get('PORT').default(3000).asPortNumber();
 export const mongoUri = env.get('MONGO_URI').required().asString();
 export const mongoDb = env.get('MONGO_DB_NAME').required().asString();
