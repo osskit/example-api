e4a05b7e1f6af975f2dc2a73aa5dc0b30b8a06ae
Sharon Grossman <sharong1337@gmail.com>
2022-07-19 14:26:07 +0300

    .

--- tests/closed-box/jest.config.ts
+++ tests/closed-box/jest.config.ts
@@ -8,7 +8,7 @@ const config: InitialOptionsTsJest = {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     'ts-jest': {
       useESM: true,
-      useIsolatedModules: true,
+      isolatedModules: true,
       diagnostics: {
         exclude: ['!**/*.spec.ts'],
       },
