479f7fca8b844bfc535cf4993a54d7214e85334c
Sharon Grossman <sharong1337@gmail.com>
2022-07-19 15:10:17 +0300

    .

--- scripts/generate-openapi.ts
+++ scripts/generate-openapi.ts
@@ -1,6 +1,8 @@
 process.env.NODE_ENV = 'dev';
 process.env.MONGO_URI = 'bla';
 process.env.MONGO_DB_NAME = 'bla';
+process.env.SERVICE_NAME = 'bla'
+process.env.SERVICE_VERSION = 'bla'
 
 const { init } = await import('../src/server/index.js');
 const fastify = await init();
